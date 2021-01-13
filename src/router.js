160035e9cef4df71d7da9e40eb846ea12304dc9f
VictorOnwukwe <victoronwukwe25@gmail.com>
2021-01-13 11:25:06 +0100

    Implement route by gender

--- src/router.js
+++ src/router.js
@@ -7,16 +7,39 @@ import Users from "./pages/Users.vue";
 Vue.use(Router);
 
 export default new Router({
-    routes: [
-        {
-            path: '/',
-            component: Users,
-            name: "users",
+  routes: [
+    {
+      path: "/",
+      component: Users,
+      name: "all users",
+      props: {
+        gender: "all",
+      },
+      meta: {isUsers: true}
+    },
+    {
+      path: "/male",
+      component: Users,
+      name: "male users",
+      props: {
+        gender: "male",
+      },
+      meta: {isUsers: true}
+    },
+    {
+        path: "/female",
+        component: Users,
+        name: "female users",
+        props: {
+          gender: "female",
         },
-        {
-            path: '/user',
-            component: User,
-            name: 'user'
-        }
-    ]
-})
+        meta: {isUsers: true}
+      },
+    {
+      path: "/users/:email",
+      component: User,
+      name: "user",
+      meta: {isUsers: false}
+    },
+  ],
+});
