a55760c31487a62cd49d4611f3ca72056c583e33
VictorOnwukwe <victoronwukwe25@gmail.com>
2021-01-13 11:21:58 +0100

    Modify input component

--- src/components/Input.vue
+++ src/components/Input.vue
@@ -1,12 +1,36 @@
 <template>
   <div
-    style="display:flex;align-items:center;padding:10px"
-    :style="{ backgroundColor: color }"
+    class="input-body"
+    :class="disabled ? 'disabled' : null"
+    @click="focus()"
+    :style="{
+      backgroundColor: backgroundColor,
+      borderRadius: radius,
+      padding: padding,
+    }"
   >
-    <i :class="icon.name" :style="{ color: icon.color }"></i>
-    <div style="position:relative">
-      <span style="position:absolute">{{ placeholder.text }}</span>
-      <input class="input" type="text" />
+    <i :class="icon" :style="{ color: iconColor }"></i>
+    <div style="position:relative;flex-grow:1;text-overflow:ellipsis;">
+      <div style="position:absolute;left:16px;text-overflow:ellipsis;white-space:nowrap">
+      <span
+        style="letter-spacing: -0.02px;text-wrap:nowrap"
+        :style="{
+          fontSize: placeholderSize,
+          fontWeight: placeholderWeight,
+          top: `calc((80% - ${placeholderSize}) / 2)`,
+          color: placeholderColor,
+        }"
+        v-show="input.length == 0"
+        >{{ placeholder }}</span
+      ></div>
+      <input
+        :disabled="disabled"
+        :id="id"
+        :style="{ color: color }"
+        v-model="input"
+        @keyup="emit"
+        type="text"
+      />
     </div>
   </div>
 </template>
@@ -15,30 +39,81 @@ export default {
   name: "m-input",
   props: {
     icon: {
-      type: Object,
-      default: () => {
-        return {
-          name: "fas fas-search",
-          color: "rgb(180,180,180)",
-        };
-      },
+      type: String,
+      default: "fas fa-search",
+    },
+    iconColor: {
+      type: String,
+      default: "rgba(0,0,0,0.62)",
     },
     placeholder: {
-      type: Object,
-      default: () => {
-        return {
-          text: "Enter text here",
-          color: "rgb(180,180,180)",
-        };
-      },
+      type: String,
+      default: "Enter text here",
     },
-    color: String,
+    placeholderColor: {
+      type: String,
+      default: "rgba(0,0,0,0.8)",
+    },
+    placeholderSize: {
+      type: String,
+      default: "14px",
+    },
+    placeholderWeight: {
+      type: String,
+      default: "normal",
+    },
+    color: {
+      type: String,
+      default: "rgba(0,0,0,.87)",
+    },
+    backgroundColor: {
+      type: String,
+      default: "#FFFFFF",
+    },
+    radius: {
+      type: String,
+      default: "5px",
+    },
+    padding: {
+      type: String,
+      default: "10px",
+    },
+    id: String,
+    disabled: Boolean,
+  },
+  data() {
+    return {
+      input: "",
+      showPlaceholder: true
+    };
+  },
+  methods: {
+    focus() {
+      document.querySelector("#" + this.id).focus();
+    },
+    emit(e){
+      this.$emit('input', e.target.value);
+    }
   },
 };
 </script>
 <style scoped>
 input {
   border: none;
-  background-color: rgba(0,0,0,0);
+  background-color: rgba(0, 0, 0, 0);
+  position: relative;
+  padding-left: 16px;
+  width: 100%;
+  outline: none;
+}
+.input-body {
+  display: flex;
+  align-items: center;
+  cursor: text;
+  position:relative;
+  overflow:hidden;
+}
+.input-body.disabled {
+  cursor: default;
 }
 </style>
